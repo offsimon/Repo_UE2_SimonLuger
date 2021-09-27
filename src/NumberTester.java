d6748355f65b66e83ef07f939aec1d56454a4068
offsimon <lugers@190089@sus.htl-grieskirchen.at>
2021-09-27 11:16:22 +0200

    created class NumberTester and interface NumberTest

--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,2 +1,26 @@
 public class NumberTester {
+
+    private String filePath;
+
+    public NumberTester(String fileName) {
+        filePath = fileName;
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+        //TODO
+    }
+
+
 }
